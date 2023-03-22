9a7299b1dd53a643d214f9cfbc301f0eebba8a4d
abrahamnyuiadzi <113628163+abrahamnyuiadzi@users.noreply.github.com>
2023-03-22 15:22:09 +0000

    Update Program.cs

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -2,8 +2,36 @@ using System;
 
 class Program
 {
+ 
+    int choice;
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+          
+        Console.WriteLine(@"what is your choose between 1-5);
+        .......1.create a new goal
+        ........2.Lists Goals
+        ........3.Save goals 
+        ........4 Load goals
+                ........5.record event 
+        ........4 Exit 
+        ");
+
+        System.Console.WriteLine("what is your choice ");
+        String userInput =Console.ReadLine();
+
+          
+           
+         
+
+       
+
+          if (userInput == "1") {
+
+
+           
+
+          }
+    
     }
+  
 }
