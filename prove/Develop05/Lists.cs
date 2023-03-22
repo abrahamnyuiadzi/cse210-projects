858bb01af7b620af763385481a1a6c1942bdfe79
abrahamnyuiadzi <abrahamnyuiadzi@gmail.com>
2023-03-22 15:20:58 +0000

    Fixed problems where nothing was displayed on the screen.

--- prove/Develop05/Lists.cs
+++ prove/Develop05/Lists.cs
@@ -1,3 +1,16 @@
 class Lists{
+
     
+   string filename = "myFile.txt";
+string[] lines = System.IO.File.ReadAllLines(filename);
+
+foreach (string line in lines)
+{
+    string[] parts = line.Split(",");
+
+    string firstName = parts[0];
+    string lastName = parts[1];
+
+
+} 
 }
--- prove/Develop05/Load.cs
+++ prove/Develop05/Load.cs
@@ -1,4 +1,23 @@
+
+
+
 class Load{
 
+    using System.IO; 
+
+...
+
+
+
+string fileName = "myFile.txt";
+
+using (StreamWriter outputFile = new StreamWriter("myFile.txt"))
+{
+    // You can add text to the file with the WriteLine method
+    outputFile.WriteLine("This will be the first line in the file.");
     
+    // You can use the $ and include variables just like with Console.WriteLine
+    string color = "Blue";
+    outputFile.WriteLine($"My favorite color is {color}");
+}
 }
