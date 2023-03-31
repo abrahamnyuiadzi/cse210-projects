5442a49bf50c7c50160f17745d35b227a39e1080
abrahamnyuiadzi <abrahamnyuiadzi@gmail.com>
2023-03-31 15:50:22 +0000

    Fixed problems where nothing was displayed on the screen.

---
+++ final/own/GenerateConfig.cs
@@ -0,0 +1,19 @@
+namespace PasswordGenerator{
+
+
+
+    internal class GenerateConfig
+    {
+
+        public int length  {get ;  set ;}
+
+        public bool IsLowerCase {get ;set;  }
+
+        public bool IsUpperCase{get ;set ;}
+
+        public bool IsDigits {get ; set ;}
+        public bool IsSymbols{get; set; }
+
+       
+    }
+}
